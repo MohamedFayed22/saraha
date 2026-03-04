0b58c2f596de4c6d436883b5b7d7bb936b0b1233
MohamedFayed <32162186+MohamedFayed22@users.noreply.github.com>
2026-03-04 11:04:00 +0300

    update 5

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -4,9 +4,10 @@ import { authentication } from "../../common/middleware/authentication.js";
 import { authorization } from "../../common/middleware/authorization.js";
 import { roleEnum } from "../../common/enum/user.enum.js";
 import {validate} from "../../common/middleware/validation.js";
+import {multer_local} from "../../common/middleware/multer.js";
 const userRouter = Router();
 
-userRouter.post("/signup",validate, US.signUp);
+userRouter.post("/signup",multer_local().single("attachment"),validate, US.signUp);
 userRouter.post("/signin", US.signIn);
 userRouter.get(
   "/profile/:id",
